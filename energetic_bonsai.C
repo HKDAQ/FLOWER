6d430f935b2e9fd82d3653b2d5267b29f8cf6284
Jost Migenda <jmigenda1@sheffield.ac.uk>
2019-02-11 19:14:42 +0000

    add bool to switch between HK and SK; remove unrelated histograms; get PMT position only once needed

--- energetic_bonsai.C
+++ energetic_bonsai.C
@@ -20,16 +20,9 @@
 #endif
 
 // low energy reconstruction
-int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
-	// set up histograms
-	//TH1F *actX = new TH1F("Actual X", "Actual X", 200, -1500, 1500);
-	TH1F *recTheta = new TH1F("Reconstructed Theta", "Reconstructed Theta", 35, -3.500, 3.500);
-	TH1F *recPhi = new TH1F("Reconstructed Phi", "Reconstructed Phi", 35, -3.500, 3.500);
-	TH1F *recAlpha = new TH1F("Reconstructed Alpha", "Reconstructed Alpha", 35, -3.500, 3.500);
-	TH1F *recCherenkov = new TH1F("Reconstructed Cherenkov angle", "Reconstructed Cherenkov angle", 100, -1, 1);
+int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false, bool is_HK=false) {
+	// set up histogram
 	TH1F *recEnergy = new TH1F("Reconstructed Energy", "Reconstructed Energy", 50, 0, 100);
-	//TH1F *recEllipticity = new TH1F("Reconstructed ellipticity", "Reconstructed ellipticity", 100, -1, 1);
-	//TH1F *recLikelihood = new TH1F("Reconstructed likelihood", "Reconstructed likelihood", 100, -100, 900);
 
 #if !defined(__MAKECINT__)
 	// Load the library with class dictionary info (create with "gmake shared")
@@ -74,13 +67,13 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 	int bsNsel[2];
 
 	// used for energy reconstruction
-	float x,y,z, ratio, occupancy, ttmp, tStart, lateHits, darkRate, darkNoise, dotProduct, theta, photoCoverage, waterTransparency, nEffHit, nEff, eRec;
 	int i, j, ncherenkovdigihits, n50tmp, n100tmp, n50Max, n100Max, nPMTs, nWorkingPMTs, nearbyHits;
+	float x,y,z, ratio, occupancy, ttmp, tStart, lateHits, darkRate, darkNoise, dotProduct, theta, photoCoverage, waterTransparency, nEffHit, nEff, eRec;
 	float lambdaEff = 100*100; // scattering length in cm (based on Design Report II.2.E.1)
 	const float effCoverages[] = {0.4, 0.4, 0.4, 0.4, 0.4068, 0.4244, 0.4968, 0.5956, 0.67}; // from MC: coverage at theta = 5, 15, ..., 85 degree
 	WCSimRootPMT pmt, otherpmt;
 
-	// Now loop over events
+	// Loop over events
 	for (int ev=0; ev < tree->GetEntries(); ev++) {
 		if (verbose) std::cout << "event number: " << ev << std::endl;
 
@@ -93,6 +86,9 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 
 		// Loop over triggers in the event
 		for (int index = 0 ; index < event->GetNumberOfEvents(); index++) {
+			// TODO: NDigits trigger in WCSim is not optimized for HK, sometimes adds a second trigger for noise. We ignore those here.
+			if (index == 1) continue;
+
 			trigger = event->GetTrigger(index);
 			ncherenkovdigihits = trigger->GetNcherenkovdigihits();
 			if (ncherenkovdigihits == 0) {
@@ -103,14 +99,11 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 			std::vector<float> bsQ (ncherenkovdigihits,0);
 			std::vector<float> distance (ncherenkovdigihits,0);
 			std::vector<float> tCorrected (ncherenkovdigihits,0);
-			std::vector<float> PMTX (ncherenkovdigihits,0);
-			std::vector<float> PMTY (ncherenkovdigihits,0);
-			std::vector<float> PMTZ (ncherenkovdigihits,0);
 			std::vector<int> bsCAB (ncherenkovdigihits,0);
 
 			bsNhit = & ncherenkovdigihits;
 
-			// get time, charge and PMT number for each WCSimRootCherenkovDigiHit in the trigger
+			// Get time, charge and PMT number for each hit
 			for (i=0; i<ncherenkovdigihits; i++) {
 				TObject *element = (trigger->GetCherenkovDigiHits())->At(i);
 				WCSimRootCherenkovDigiHit *cherenkovdigihit = dynamic_cast<WCSimRootCherenkovDigiHit*>(element);
@@ -118,27 +111,14 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 				bsT[i] = cherenkovdigihit->GetT();
 				bsQ[i] = cherenkovdigihit->GetQ();
 				bsCAB[i] = cherenkovdigihit->GetTubeId();
-
-				pmt = geo->GetPMT(cherenkovdigihit->GetTubeId());
-				PMTX[i] = pmt.GetPosition(0);
-				PMTY[i] = pmt.GetPosition(1);
-				PMTZ[i] = pmt.GetPosition(2);
-			} // End of loop over Cherenkov digihits
+			}
 
 			// fit vertex position and direction using BONSAI
 			int* bsCAB_a = &bsCAB[0]; // turn std::vector into a pointer for BONSAI
 			float* bsT_a = &bsT[0];
 			float* bsQ_a = &bsQ[0];
 
-			bonsai->BonsaiFit( bsVertex, bsResult, bsGood, bsNsel, bsNhit, bsCAB_a, bsT_a, bsQ_a);
-
-			// fill histograms with reconstructed vertex position (bsVertex[i]) and direction (bsResult[i])
-			recTheta->Fill(bsResult[0]);
-			recPhi->Fill(bsResult[1]);
-			recAlpha->Fill(bsResult[2]);
-			recCherenkov->Fill(bsResult[3]);
-			//recEllipticity->Fill(bsResult[4]);
-			//recLikelihood->Fill(bsResult[5]);
+			bonsai->BonsaiFit(bsVertex, bsResult, bsGood, bsNsel, bsNhit, bsCAB_a, bsT_a, bsQ_a);
 
 
 			// ****************************************
@@ -149,7 +129,11 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 
 			// Correct all hit times for time-of-flight between vertex and PMT
 			for (i=0; i<ncherenkovdigihits; i++) {
-				distance[i] = sqrt(pow((PMTX[i]-bsVertex[0]), 2) + pow((PMTY[i]-bsVertex[1]), 2) + pow((PMTZ[i]-bsVertex[2]), 2));
+				pmt = geo->GetPMT(bsCAB[i]);
+				x = pmt.GetPosition(0);
+				y = pmt.GetPosition(1);
+				z = pmt.GetPosition(2);
+				distance[i] = sqrt(pow((x-bsVertex[0]), 2) + pow((y-bsVertex[1]), 2) + pow((z-bsVertex[2]), 2));
 				tCorrected[i] = bsT[i] - (distance[i]/21.58333); // speed of light in water, value from https://github.com/hyperk/hk-BONSAI/blob/d9b227dad26fb63f2bfe80f60f7f58b5a703250a/bonsai/hits.h#L5
 			}
 
@@ -182,7 +166,7 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 			std::vector<float> distance50 (n50Max,0);
 			std::vector<int> tubeID (n50Max,0);
 			j=0;
-			for (i=0; i<ncherenkovdigihits; i++) { // loop over tCorrected, which is in the same order as the vectors PMTX/Y/Z and bsCAB
+			for (i=0; i<ncherenkovdigihits; i++) { // loop over tCorrected, which is in the same order as the vectors distance and bsCAB
 				if (tStart <= tCorrected[i] && tCorrected[i] < tStart + 50) {
 					distance50[j] = distance[i];
 					tubeID[j] = bsCAB[i];
@@ -199,6 +183,11 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 			// TODO: these are detector-dependent
 			nPMTs = 11146; // total number of PMTs
 			darkRate = 4.2/1000000; // dark noise rate (per ns) of the PMT (8.4kHz for B&L PMT)
+			if (is_HK) {
+				nPMTs = 38448;
+				darkRate = 8.4/1000000;
+			}
+
 			nWorkingPMTs = nPMTs; // number of working PMTs (may be lower in real detector due to defects)
 			nEff = 0; // effective number of hits
 
@@ -269,7 +258,6 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 
 			// reconstruct energy from nEff; this is approximately linear, except at very low energies
 			// TODO: determine fit parameters
-			float eRec = 0;
 // 			float a[5]= {0.82, 0.13, -1.11*pow(10, -4), 1.25*pow(10, -6), -3.42*pow(10, -9)};
 // 			if (nEff<189.8) {
 // 				for (int n=0;n<5;n++) {
@@ -278,6 +266,10 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 // 			} else {
 				eRec=(25.00 + 0.138*(nEff-189.8))*0.378; // TODO: dummy value; needs to be determined/tested much more precisely!
 // 			}
+			if (is_HK) {
+				eRec = eRec /2.584; // TODO: HK observes more photons for a given energy due to different PMTs etc.
+			}
+
 			recEnergy->Fill(eRec);
 
 			if (verbose) {
@@ -292,17 +284,12 @@ int energetic_bonsai(char *filename="../wcsim.root", bool verbose=false) {
 
 	// display histograms
 	float winScale = 0.75;
-	int nWide = 2;
-	int nHigh = 2;
+	int nWide = 1;
+	int nHigh = 1;
 	TCanvas* c1 = new TCanvas("c1", "First canvas", 500*nWide*winScale, 500*nHigh*winScale);
 	c1->Draw();
 	c1->Divide(nWide, nHigh);
-	c1->cd(1); recTheta->Draw();
-	c1->cd(2); recPhi->Draw();
-	c1->cd(3); recEnergy->Draw();
-	c1->cd(4); recCherenkov->Draw();
-	//c1->cd(5); recEllipticity->Draw();
-	//c1->cd(6); recLikelihood->Draw();
+	c1->cd(1); recEnergy->Draw();
 
 	return 0;
 }
